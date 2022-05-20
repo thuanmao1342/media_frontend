b40b25f11ea633d4c6fa7f1c3d0a8d122eb7a9c0
nvtgitlab <thuanmao1342@gmail.com>
2022-05-20 17:22:51 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './layout/Layout';
 import BackDrop from './pages/notfound/Backdrop';
 
 const Login = lazy(() => import('./pages/auth/login/Login'));
 const NotFound = lazy(() => import('./pages/notfound/NotFound'));
+const Layout = lazy(() => import  ('./layout/Layout'));
+const Register = lazy(() => import ('./pages/auth/register/Register'));
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Suspense fallback={<BackDrop />}><Layout /></Suspense>} />
         <Route path="/login" element={<Suspense fallback={<BackDrop />}><Login /></Suspense>} />
+        <Route path="/register" element={<Suspense fallback={<BackDrop />}><Register /></Suspense>} />
         <Route
           path="*"
           element={<Suspense fallback={<BackDrop />}><NotFound /></Suspense>}
---
+++ src/pages/auth/register/Register.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import styles from './register.module.scss';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Button, Checkbox, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
+
+const schema = yup.object({
+  username: yup.string().required("Username is required!").trim(),
+  password: yup.string().required("Password is required!").trim().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^ ]{6,20}$/, "Password must be at least 6 characters, contain at least one lowercase letter, one uppercase letter and one number!"),
+}).required();
+
+export default function Register() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const navigate = useNavigate();
+  const onSubmit = data => {
+    navigate("/");
+    console.log(data);
+  }
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box>
+      <Grid container spacing={0} direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={7}>
+          <div className={styles.bg_img}>
+          </div>
+        </Grid>
+        <Grid item xs={5} className={styles.form}>
+          <Typography variant='h3'>Sign in</Typography>
+          <Box sx={{ p: 6 }}>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.from_login}>
+              <Stack spacing={4}>
+                <TextField
+                  {...register("fullname")}
+                  error={errors.username ? true : false}
+                  id="Fullname"
+                  label="Fullrname"
+                  fullWidth
+                  helperText={errors.username?.message}
+                  required
+                />
+                <FormControl fullWidth>
+                  <InputLabel>Gender</InputLabel>
+                  <Select
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>Male</MenuItem>
+                    <MenuItem value={0}>Female</MenuItem>
+                    <MenuItem value={3}>Other</MenuItem>
+                  </Select>
+                </FormControl>
+                <TextField
+                  {...register("username")}
+                  error={errors.username ? true : false}
+                  id="username"
+                  label="Email or username"
+                  fullWidth
+                  helperText={errors.username?.message}
+                  required
+                />
+                <TextField
+                  {...register("password")}
+                  error={errors.password ? true : false}
+                  id="password"
+                  label="Password"
+                  fullWidth
+                  helperText={errors.password?.message}
+                  required
+                />
+                <TextField
+                  {...register("cfpassword")}
+                  error={errors.password ? true : false}
+                  id="cfpassword"
+                  label="Confirm Password"
+                  fullWidth
+                  helperText={errors.password?.message}
+                  required
+                />
+              </Stack>
+              <div className={styles.footer_form}>
+                <Checkbox {...register("remember")} defaultChecked /> <label>Remember me</label>
+                <Button type='submit' variant="contained">
+                  Login
+                </Button>
+                <div className={styles.link}>
+                  <a href='/register'>create a account</a>
+                  <a href='/refeshpassword'>forgot password</a>
+                </div>
+              </div>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box >
+  )
+}
